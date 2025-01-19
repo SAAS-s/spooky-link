6ed9638a3011372ffa7a384e2da4ada984f13c5f
SAAS-s <surmai10131@gmail.com>
2025-01-19 18:30:50 -0500

    test code

---
+++ minimal_logger.py
@@ -0,0 +1,17 @@
+from burp import IBurpExtender, IHttpListener
+
+class BurpExtender(IBurpExtender, IHttpListener)
+    def registerExtenderCallbacks(self, callbacks):
+        self.callbacks = callbacks
+        self.helpers = callbacks.getHelpers()
+        callbacks.setExtensionName("Minimal Logger")
+        callbacks.registerHttpListener(self)
+
+    def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
+        if messageIsRequest:
+            request = self.helpers.bytesToString(messageInfo.getRequest())
+            print("[Request]:\n", request)
+        else:
+            response = self.helpers.bytesToString(messageInfo.getResponse())
+            print("[Response]:\n", response)
+
